05b31ca0b9511c07b66d82453fac7732aff42923
Florian <florianbarth24@gmail.com>
2025-06-09 21:22:13 +0200

    less outputs

--- run.sh
+++ run.sh
@@ -2,11 +2,10 @@ clear &&
 rm -rf rfx &&
 mkdir rfx &&
 cd rfx &&
-wget https://github.com/java3east/RefineXModular/releases/download/alpha-1.0/refinex.zip &&
-unzip refinex.zip &&
+wget -q https://github.com/java3east/RefineXModular/releases/download/alpha-1.0/refinex.zip &&
+unzip -qq refinex.zip &&
 rm refinex.zip &&
 cd .. &&
-ls -la &&
 java -jar ./rfx/RefineX-1.0-SNAPSHOT.jar RFX ./__test/run.lua &&
 rm -rf rfx &&
 echo "RefineX run completed."
