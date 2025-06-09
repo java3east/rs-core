a9ba643f00141b66a5b0afab4f15e2e44aecc129
Florian <florianbarth24@gmail.com>
2025-06-09 18:52:16 +0200

    :ambulance: pipeline fix

--- run.sh
+++ run.sh
@@ -1,1 +1,11 @@
-clear && java -jar /home/florian/Documents/github/RefineX2/RefineX-1.0-SNAPSHOT.jar RFX ./__test/run.lua && cd ..
+clear
+rm -rf rfx
+mkdir rfx
+cd rfx
+wget https://github.com/java3east/RefineXModular/releases/download/alpha-1.0/refinex.zip
+unzip refinex.zip
+rm refinex.zip
+cd ..
+java -jar ./rfx/RefineX-1.0-SNAPSHOT.jar RFX ./__test/run.lua
+rm -rf rfx
+echo "RefineX run completed."
