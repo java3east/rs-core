e0f80dff8fe5edc7bfb52b36ef7d1258281644af
Florian Barth <beastofjava7@gmail.com>
2025-06-09 13:24:38 +0200

    Add RFX.Simulation class and SIMULATION_CREATE function definition

---
+++ rfx_meta.lua
@@ -0,0 +1,9 @@
+---@meta RFX
+
+---@class RFX.Simulation
+
+---Creats a new simulation of the given type.
+---@nodiscard
+---@param type 'HELIX'|'FIVEM'
+---@return RFX.Simulation simulation
+function SIMULATION_CREATE(type) end
