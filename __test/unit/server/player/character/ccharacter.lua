292ab77ed8b7d5c8b6997c5aa9cda5a0c23e8b5d
Florian <florianbarth24@gmail.com>
2025-06-11 10:47:57 +0200

    :sparkles: added ccharacter tests

--- __test/unit/server/player/character/ccharacter.lua
+++ __test/unit/server/player/character/ccharacter.lua
@@ -10,4 +10,34 @@ Test.new('CCharacter should exist', function()
     return Test.assert(CCharacter ~= nil, "CCharacter should not be nil")
 end)
 
+Test.new('CCharacter should generate new citizenId if not provided', function()
+    -- when
+    local cCharacter = CCharacter.new()
+
+    -- then
+    return Test.assert(cCharacter.citizenId ~= nil and cCharacter.citizenId ~= "", "CCharacter should generate a new citizenId if not provided")
+end)
+
+Test.new('CCharacter should use provided citizenId', function()
+    -- given
+    local id = "test_citizen_id"
+
+    -- when
+    local cCharacter = CCharacter.new(id)
+
+    -- then
+    return Test.assert(cCharacter.citizenId == id, "CCharacter should use the provided citizenId")
+end)
+
+Test.new('CCharacter get data should return correct data', function (self)
+    -- given
+    local cCharacter = CCharacter.new()
+
+    -- when
+    local data = cCharacter:getData()
+
+    -- then
+    return Test.assert(type(data.citizenId) == "string", "data.citizenId should be a string")
+end)
+
 Test.runAll("Server.CCharacter")
